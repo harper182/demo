cadbbe395249b6bbbf9fe5e9258c56ca88c6f664
harper182 <982255390@qq.com>
2019-03-27 17:26:45 +0800

    init

--- src/main/java/com/example/demo/api/OrderController.java
+++ src/main/java/com/example/demo/api/OrderController.java
@@ -1,4 +1,18 @@
 package com.example.demo.api;
 
+import com.example.demo.entity.Order;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController("/order")
 public class OrderController {
+
+    @GetMapping
+    public List<Order> list(){
+        return new ArrayList<>();
+    }
+
 }
